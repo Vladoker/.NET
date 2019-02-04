d5f54dd0dfdcd2f5d7cf1560745aacd5daa6a78f
Vladoker <45948200+Vladoker@users.noreply.github.com>
2019-02-04 18:27:15 +0200

    update 04.02

---
+++ File_input_output/ProductStore Учителя/ProductStore.Common/Constants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStore.Common
+{
+    public static class Constants
+    {
+        public static class ProductDays
+        {
+            public const int ValidPeriod = 3;
+        }
+
+        public const string ProductStorePath = @"D:\GR1724\Projects\ProductStore\DATA\Products.txt";
+        public const string OwnerStorePath = @"D:\GR1724\Projects\ProductStore\DATA\Owners.txt";
+    }
+}
---
+++ File_input_output/ProductStore Учителя/ProductStore.Core/Managers/OwnerManager.cs
@@ -0,0 +1,35 @@
+﻿using ProductStore.Entities;
+using ProductStore.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductStore.Core
+{
+    public class OwnerManager
+    {
+        private OwnerRepository ownerRepository;
+
+        public OwnerManager(OwnerRepository ownerRepository)
+        {
+            this.ownerRepository = ownerRepository;
+        }
+
+        public Owner AddOwner(Owner owner)
+        {
+            return ownerRepository.AddOwner(owner);
+        }
+
+        public Owner GetOwner(Guid ownerId)
+        {
+            return ownerRepository.GetOwner(ownerId);
+        }
+        public List<Owner> GetOwners()
+        {
+            return ownerRepository.GetOwners();
+        }
+
+    }
+}
