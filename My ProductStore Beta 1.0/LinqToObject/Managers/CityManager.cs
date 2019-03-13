0568f8c5b2bea67d858bcfca5c1d44748c84d2f6
Vladoker <45948200+Vladoker@users.noreply.github.com>
2019-03-13 19:02:37 +0200

    update LINQ Search

---
+++ My ProductStore Beta 1.0/LinqToObject/Managers/CityManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqToObject.Managers
+{
+    public class CityManager
+    {
+
+        private readonly List<string> cities = new List<string>()
+        {
+              "Кишинёв",
+                "Киев",
+                "Копенгаген",
+                "Мумбаи",
+                "Флорианополис",
+                "Сиэтл",
+                "Осло",
+                "Тель-Авив",
+                "Лиссабон",
+                "Кали",
+                "Окленд",
+                "Стамбул",
+                "Ханой",
+                "Токио",
+                "Тайбэй",
+                "Рейкьявик",
+                "Берлин",
+                "Кейптаун",
+                "Лондон",
+                "Торонто",
+        };
+
+        public List<string> SearchCities(char startLetter, char endLetter)
+        {
+            var result = from c in cities where Char.ToUpper(c.ToArray().First()) == Char.ToUpper(startLetter)
+                         && Char.ToUpper(c.ToArray().Last()) == Char.ToUpper(endLetter)
+                         select c;
+
+            return result.ToList();
+        }
+
+    }
+}
+
+//Console.Write("Please enter first letter of city :");
+//            char startLetter = Console.ReadKey().KeyChar;
+
+//Console.WriteLine();
+//            Console.Write("Please enter last letter of city :");
+//            char endLetter = Console.ReadKey().KeyChar;
+
+
+//var cityManager = new CityManager();
+
+//var cities = cityManager.SearchCities(startLetter, endLetter);
+
+//            if (!cities.Any())
+//            {
+//                Console.WriteLine("Data not found");
+//                Console.ReadKey();
+//                return;
+//            }
+
+//            Console.WriteLine();
+//            foreach (var city in cities)
+//            {
+//                Console.WriteLine(city);
+//            }
+
+//           Console.ReadKey();
--- My ProductStore Beta 1.0/LinqToObject/Program.cs
+++ My ProductStore Beta 1.0/LinqToObject/Program.cs
@@ -10,7 +10,49 @@ namespace LinqToObject
     {
         static void Main(string[] args)
         {
-            SearchCity();
+            
+        }
+
+        private static void GroupedContractsLINQ()
+        {
+            var contracts = new List<Contract>
+            {
+             new Contract { ContractId = 1, ContracDate = DateTime.Now.AddDays(-1), Status = Domain.Enums.ContractStatus.Pending },
+               new Contract { ContractId = 2, ContracDate = DateTime.Now.AddDays(-3), Status =  Domain.Enums.ContractStatus.Opened },
+               new Contract { ContractId = 3, ContracDate = DateTime.Now.AddDays(1), Status =  Domain.Enums.ContractStatus.Closed },
+               new Contract { ContractId = 4, ContracDate = DateTime.Now.AddDays(5) , Status =  Domain.Enums.ContractStatus.Closed},
+               new Contract { ContractId = 5, ContracDate = DateTime.Now.AddDays(2), Status =  Domain.Enums.ContractStatus.Opened }
+
+            };
+
+            var invoices = new List<Invoice>
+            {
+                new Invoice{InvoiceId = Guid.NewGuid(), ContractId = 5, Total = 100.00M },
+                new Invoice{InvoiceId = Guid.NewGuid(), ContractId = 4, Total = 200.00M },
+                new Invoice{InvoiceId = Guid.NewGuid(), ContractId = 3, Total = 300.00M },
+                new Invoice{InvoiceId = Guid.NewGuid(), ContractId = 2, Total = 400.00M },
+                new Invoice{InvoiceId = Guid.NewGuid(), ContractId = 1, Total = 500.00M }
+            };
+
+            var groupedContracts = from c in contracts
+                                   group c by c.Status into ContractStatusGroup
+                                   select new
+                                   {
+                                       ContractStatus = ContractStatusGroup.Key,
+                                       Contract = ContractStatusGroup.ToList()
+                                   };
+
+            foreach (var gc in groupedContracts)
+            {
+                Console.WriteLine(gc.ContractStatus);
+                foreach (var item in gc.Contract)
+                {
+                    Console.WriteLine($"ContractId => {item.ContractId} ContractDate => {item.ContracDate} Status => {item.Status}");
+                }
+            }
+
+
+            Console.ReadKey();
         }
 
         private static void SearchCity()
